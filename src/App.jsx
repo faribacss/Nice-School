6baffcd8ca80953b02ca9694f64b0ef18d92118d
Fariba Shahsavan <f.shahsavanpour@gmail.com>
2025-05-13 14:09:47 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="container py-5">
+      <h1 className="text-center text-primary">Welcome to Nice School</h1>
+      <p className="lead text-center">This is a React + Vite + Bootstrap project ready for GitHub Pages.</p>
+    </div>
+  )
+}
+
+export default App
