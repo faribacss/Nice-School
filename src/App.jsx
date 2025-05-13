011d3be9275a73ad439b1206c28150f1d47abc6b
Fariba Shahsavan <f.shahsavanpour@gmail.com>
2025-05-13 18:04:09 +0330

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="container py-5">
-      <h1 className="text-center text-primary">Welcome to Nice School</h1>
-      <p className="lead text-center">This is a React + Vite + Bootstrap project ready for GitHub Pages.</p>
-    </div>
-  )
-}
+    <>
+      <Header />
+    </>
+  );
+} 
 
-export default App
+export default App;
